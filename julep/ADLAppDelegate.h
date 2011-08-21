58efcbac58d78e1a1fa5ba9f55e2169e2a4c9611
Akiva Leffert <aleffert@ananda.local>
2011-08-21 14:27:05 -0700

    Initial Commit

---
+++ julep/ADLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ADLAppDelegate.h
+//  julep
+//
+//  Created by Akiva Leffert on 8/21/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ADLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ julep/ADLViewController.h
@@ -0,0 +1,13 @@
+//
+//  ADLViewController.h
+//  julep
+//
+//  Created by Akiva Leffert on 8/21/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ADLViewController : UIViewController
+
+@end
